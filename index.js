d66686d8a3ea472d13fd3b8d701fa094040cf6ca
ritik-gupta3010 <ritikgupta30102000@gmail.com>
2023-03-10 23:11:03 +0530

    add comment

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const path = require('path')
 
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
+// const router1 = jsonServer.router(path.join(__dirname, 'comment.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
-
+// server.use(router1)
 const PORT = 8000
 
 server.listen(PORT, () => {
